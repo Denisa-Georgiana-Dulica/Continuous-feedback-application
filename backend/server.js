bb1273b3be78eb5a3bef94b265110cd8d63d49c5
Dulica Denisa <dulica.denisa13@gmail.com>
2024-12-01 00:03:31 +0200

    Login

--- backend/server.js
+++ backend/server.js
@@ -26,22 +26,17 @@ app.post('/login', async (req, res) => {
     const { email, password } = req.body;
     console.log(req.body);
 
-    // Verificăm dacă email-ul și parola au fost furnizate
     if (!email || !password) {
         return res.status(400).json({ error: 'Email and password are required' });
     }
 
     try {
-        // Căutăm utilizatorul în baza de date după email
         const foundUser = await user.findOne({ where: { email } });
 
         if (!foundUser) {
             return res.status(404).json({ error: 'User not found' });
         }
 
-        // Verificăm dacă parola introdusă corespunde cu parola din baza de date
-        // Dacă parolele sunt criptate, folosim bcrypt.compare() pentru a le compara
-        // Aici presupunem că parola din baza de date este deja criptată cu bcrypt
         const isPasswordValid = await bcrypt.compare(password, foundUser.password);
 
         if (!isPasswordValid) {
@@ -50,12 +45,12 @@ app.post('/login', async (req, res) => {
 
         // Generăm un JWT
         const token = jwt.sign(
-            { userId: foundUser.userId, role: foundUser.role }, // Payload-ul JWT
-            process.env.JWT_SECRET,                             // Secretul pentru semnătura JWT
-            { expiresIn: '1h' }                                // Expirarea token-ului
+            { userId: foundUser.userId, role: foundUser.role }, 
+            process.env.JWT_SECRET,                             
+            { expiresIn: '1h' }                              
         );
 
-        // Trimiterea JWT către client
+        
         return res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.error('Login error:', error);
