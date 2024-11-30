392cd502ed2616263f70f8b1a3834b6efb5b1bfd
Dulica Denisa <dulica.denisa13@gmail.com>
2024-12-01 01:36:09 +0200

    Show teacher's activities

---
+++ backend/data_base/activities.model.js
@@ -0,0 +1,50 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "./data_base.js";
+import { user } from "./users.model.js";
+
+
+export const activity=sequelize.define('Activities',{
+    activitiesId:{
+        type:DataTypes.UUID,
+        primaryKey:true,
+        defaultValue:DataTypes.UUIDV4
+    },
+    title:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{notEmpty:true}
+    },
+    description:{
+        type:DataTypes.TEXT,
+        allowNull:false,
+        validate:{notEmpty:true}
+    },
+    code:{ 
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        unique:true,
+    },
+    start_date:{ 
+        type:DataTypes.DATE,
+        allowNull:false
+    },
+    end_date:{ 
+        type:DataTypes.DATE,
+        allowNull:false
+    },
+    teacherId:{
+        type:DataTypes.UUID,
+        allowNull:false,
+        references:{
+            model:user,
+            key:'userId'
+        },
+        onDelete:'SET NULL',
+        onUpdate:'CASCADE' 
+        
+    }
+   
+});
+
+user.hasMany(activity,{foreignKey:'teacherId'});
+activity.belongsTo(user,{foreignKey:'teacherId'});
---
+++ backend/server.js
@@ -0,0 +1,139 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express'
+import { getUserByEmail,migrate } from './data_base/index.js';
+import { user } from './data_base/users.model.js';
+import { activity } from './data_base/activities.model.js';
+import { expressjwt } from "express-jwt";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const app=express();
+app.use(express.json());
+migrate();
+
+app.use(expressjwt({
+    secret:jwtSecret,
+    algorithms:['HS256']
+}).unless({
+    path:['/login'] //endpointuri excluse
+}));
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+
+    
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' });
+    }
+
+    try {
+       
+        const foundUser = await user.findOne({ where: { email } });
+
+        if (!foundUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Incorrect password' });
+        }
+
+        const token = jwt.sign(
+            { userId: foundUser.userId, role: foundUser.role }, 
+            process.env.JWT_SECRET,                             
+            { expiresIn: '1h' }                               
+        );
+
+        return res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        console.error('Login error:', error);
+        return res.status(500).json({ error: 'Server error' });
+    }
+});
+
+app.post('/activities',async (req,res)=>{
+
+    const {title,description,start_date,end_date}=req.body;
+    if(!title || !description || !start_date || !end_date)
+    {
+        return res.status(400).json('All fields are required');
+    }
+    
+    try{
+    
+        if(!req.auth || req.auth.role!=='teacher')
+        {
+            return res.status(403).json({error:'Only teachers can create activities.'})
+        }
+
+        const lastActivity = await activity.findOne({ order: [['code', 'DESC']] });
+        const nextCode = lastActivity ? lastActivity.code + 1 : 1;
+        
+        const newActivity=await activity.create({
+            title,
+            description,
+            code:nextCode,
+            start_date,
+            end_date,
+            teacherId:req.auth.userId
+        });
+     
+        return res.status(201).json({message:'Activity created successfully ',activity:newActivity});
+    }
+    catch(e)
+    {
+        console.error('Error creating activity:',e);
+        return res.status(500).json({ error: e.message });
+    }
+    
+
+});
+
+app.get('/:email/user', async (req,res)=>{
+        const userEmail=req.params.email;
+        const user=await getUserByEmail(userEmail);
+        if(!user)
+        {
+            res.status(404).json({error: 'Email not found'});
+        }
+        else{
+            res.status(200).json({user:user});
+        }
+});
+
+app.get('/teacher-activities',async (req,res)=>{
+        try{
+         if(!req.auth || req.auth.role!=='teacher')
+        {
+            return res.status(403).json({error:'Only teachers can create activities.'})
+        }
+    
+        const teacher_activities=await activity.findAll({
+                where:{teacherId:req.auth.userId},
+                order:[['start_date','ASC']]
+        });
+
+        if(teacher_activities.length===0)
+        {
+            return res.status(200).json({message:'No activities for this teacher'});
+        }
+
+        res.status(200).json({message:"Teacher's activities:",teacher_activities});
+    }
+    catch(e)
+    {
+        console.log('Error fetching teacher activities:',e);
+        res.status(500).json({error:'Server error'});
+    }
+
+});
+app.listen(3001,()=>{
+    console.log('has started');
+});
